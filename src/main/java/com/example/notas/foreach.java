31fe3484a80596548f94c3c61343be278af28df3
andreabarreraudea123 <andreolivares2051@gmail.com>
2020-10-19 11:20:46 -0500

    primer commit

---
+++ src/main/java/com/example/notas/foreach.java
@@ -0,0 +1,20 @@
+package com.example.notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class foreach extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreach);
+
+
+
+
+
+    }
+}
